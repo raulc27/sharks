e0f0068b58884a505816e148def1d1a728b63063
Raul Castro <raul.neto.ter@estacio.br>
2021-08-14 13:26:13 -0300

    adding nodemon

--- models/sharks.js
+++ models/sharks.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Shark = new Schema({
+   
     name:{type:String, required: true},
     character:{type:String, required: true}
 });
