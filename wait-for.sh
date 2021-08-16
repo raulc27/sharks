b8b434c39ba46ae4f02972cd027f0ddee62f586b
Raul Castro <raul.neto.ter@estacio.br>
2021-08-15 22:39:12 -0300

    upgrading wait-for

--- wait-for.sh
+++ wait-for.sh
@@ -1,73 +1,61 @@
 #!/bin/sh
 
-# from the original: https://github.com/eficode/wait-for/blob/master/wait-for
+# original script: https://github.com/eficode/wait-for/blob/master/wait-for
 
 TIMEOUT=15
 QUIET=0
 
-echoerr(){
-    if["QUIET" -ne 1]
-      then printf "%s\n" "S*" '>&2';
-    fi
+echoerr() {
+  if [ "$QUIET" -ne 1 ]; then printf "%s\n" "$*" 1>&2; fi
 }
 
-usage(){
-    exitcode="$1"
-    cat << USAGE >&2
+usage() {
+  exitcode="$1"
+  cat << USAGE >&2
+Usage:
+  $cmdname host:port [-t timeout] [-- command args]
+  -q | --quiet                        Do not output any status messages
 
-    Usage:
-        $cmdname host:port [-t timeout] [-- command args]
-        -q | --quiet        Do not output any status messages
-        -t | --timeout=timeout  Execute command with args after the test finishes
-
-    USAGE
-        exit "$exitcode"
+  -t TIMEOUT | --timeout=timeout      Timeout in seconds, zero for no timeout
+  -- COMMAND ARGS                     Execute command with args after the test finishes
+USAGE
+  exit "$exitcode"
 }
 
-wait_for(){
-    for i in `seq $TIMEOUT`;
-    do
-        nc -z "$HOST" "$PORT" > /dev/null 2>&1
+wait_for() {
+  for i in `seq $TIMEOUT` ; do
+    nc -z "$HOST" "$PORT" > /dev/null 2>&1
 
     result=$?
-
-        if [$result -eq 0];
-        then
-            if [$# -gt 0];
-            then
-                exec "$@"
-            fi
-            exit 0
-        fi
-        sleep 1
-    done
-
-echo "Operation timed out" >&2
-
-exit 1
+    if [ $result -eq 0 ] ; then
+      if [ $# -gt 0 ] ; then
+        exec "$@"
+      fi
+      exit 0
+    fi
+    sleep 1
+  done
+    echo "Operation timed out" >&2
+  exit 1
 }
 
-while [$# -gt 0]
+while [ $# -gt 0 ]
 do
-    case "$1" in
-        *:* )
-        HOST=$(printf "%s\n" "$1" | cut -d : -f 1)
-        PORT=$(printf "%s\n" "$1" | cut -d : -f 2)
+  case "$1" in
+    *:* )
+    HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
+    PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
     shift 1
     ;;
-    -q  | --quiet)
+    -q | --quiet)
     QUIET=1
     shift 1
     ;;
     -t)
-
     TIMEOUT="$2"
-
-    if ["$TIMEOUT"=""];
-    then break;
-    fi
-
+    if [ "$TIMEOUT" = "" ]; then break; fi
     shift 2
+
     ;;
     --timeout=*)
     TIMEOUT="${1#*=}"
@@ -81,17 +69,15 @@ do
     usage 0
     ;;
     *)
-    echoerr "Unknow argument: $1"
+    echoerr "Unknown argument: $1"
     usage 1
     ;;
+  esac
+done
 
-    esac
-    done
+if [ "$HOST" = "" -o "$PORT" = "" ]; then
+  echoerr "Error: you need to provide a host and port to test."
+  usage 2
+fi
 
-    if["$HOST"="" -o "$PORT" = ""];
-        then
-            echerr "Error: you need to provide a host and port to test"
-            usage 2
-    fi
-
-    wait_for "$@"
+wait_for "$@"
